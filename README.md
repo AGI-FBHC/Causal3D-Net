f772e220b91ddeea77912f1c4c322a2b935dcaf2
CarlCypress <CarlCypress@yeah.net>
2025-03-29 11:53:24 +0800

    dev

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Causal3D-Net: A Stable Model for Pancreatic Cancer Diagnosis via Multi-Center Small-Sample Contrast-Enhanced CT
 
+## Usage
+
+### resample data
+
+```linux
+python resample_data.py --excel_path /home/huangdn/Causal3D-Net/src/dataset/dataset.xlsx --out_path /home/huangdn/Causal3D-Net/src/data/
+```
+
 
 
--- src/preprocessing/check_affine.py
+++ src/preprocessing/check_affine.py
@@ -8,7 +8,29 @@ import os
 import numpy as np
 import pandas as pd
 import nibabel as nib
+from tqdm import tqdm
 
 
+
+def check_affine():
+    dataset_excel = pd.read_excel("/home/huangdn/Causal3D-Net/src/dataset/dataset.xlsx")
+    processed_data = []
+    for _, row in tqdm(dataset_excel.iterrows(), total=dataset_excel.shape[0], desc="Processing images"):
+        image_path = row['image_path']
+        mask_path = row['mask_path']
+        cancer = row['cancer']
+        img = nib.load(image_path)
+        depth = img.shape[2]
+        affine_reshaped = img.affine[:3, :3].flatten()
+        processed_data.append([image_path, mask_path, cancer] + list(affine_reshaped) + [depth])
+    columns = ['image_path', 'mask_path', 'cancer'] + [f"affine_{i+1}" for i in range(9)] + ['depth']
+    result_df = pd.DataFrame(processed_data, columns=columns)
+
+    result_df.to_excel("/home/huangdn/Causal3D-Net/src/dataset/dataset_affine.xlsx", index=False)
+    pass
+
+
+if __name__ == "__main__":
+    check_affine()
 
 
