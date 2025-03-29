c0d0151016d9505b40116f270f1485acf2c6e2e0
CarlCypress <CarlCypress@yeah.net>
2025-03-29 17:07:09 +0800

    dev

--- src/preprocessing/check_affine.py
+++ src/preprocessing/check_affine.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import nibabel as nib
 from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
@@ -30,7 +32,52 @@ def check_affine():
     pass
 
 
+def analyze_affine_distribution():
+    df = pd.read_excel("/home/huangdn/Causal3D-Net/src/dataset/dataset_affine.xlsx")
+    z_spacings = df["affine_9"]
+
+    # 计算基本统计描述
+    descriptive_stats = z_spacings.describe()
+    print("Descriptive statistics:")
+    print(descriptive_stats)
+
+    # 绘制直方图
+    plt.figure(figsize=(10, 6))
+    sns.histplot(z_spacings, kde=True, color='blue', bins=10)
+    plt.title('Distribution of Z Spacings')
+    plt.xlabel('Z Spacings')
+    plt.ylabel('Frequency')
+    plt.show()
+
+    # 绘制箱线图
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x=z_spacings, color='lightgreen')
+    plt.title('Boxplot of Z Spacings')
+    plt.xlabel('Z Spacings')
+    plt.show()
+
+    # 输出其他可能需要的特征
+    print("\nVariance:", z_spacings.var())
+    print("Skewness:", z_spacings.skew())
+    print("Kurtosis:", z_spacings.kurt())
+    pass
+
+
+def change_mask_affine():
+    df = pd.read_excel("/home/huangdn/Causal3D-Net/src/dataset/dataset.xlsx")
+    for index, row in df.iterrows():
+        image_path = row['image_path']
+        mask_path = row['mask_path']
+        image_nii = nib.load(image_path)
+        mask_nii = nib.load(mask_path)
+        updated_mask_nii = nib.Nifti1Image(mask_nii.get_fdata(), image_nii.affine, header=image_nii.header)
+        output_mask_path = mask_path.replace(".nii.gz", "_updated.nii.gz")
+        nib.save(updated_mask_nii, mask_path)
+        break
+    pass
+
+
 if __name__ == "__main__":
-    check_affine()
+    change_mask_affine()
 
 
--- src/preprocessing/resample_data.py
+++ src/preprocessing/resample_data.py
@@ -52,16 +52,38 @@ def resample_z_direction(
 
     new_img = nib.Nifti1Image(new_data, affine=new_affine, header=nii.header)
 
+    # 更新 z 轴的相关字段
+    new_img.header['dim'][3] = new_data.shape[2]  # 更新 z 方向的维度
+    new_img.header['pixdim'][3] = spacing  # 更新 z 轴体素间距
+    new_img.header['srow_z'] = new_affine[2, :4]  # 更新 srow_z（仿射矩阵的第 3 行）
+
     nib.save(new_img, output_file)
 
     print(f'resampling {nii_path} completed.') if is_print else None
     return original_depth
 
 
+def get_z_spacing_list(resample_num):
+    if resample_num == 1:
+        return list()
+    elif resample_num == 3:
+        return [1, 3, 5]
+    elif resample_num == 5:
+        return [1, 2, 3, 4, 5]
+    pass
+
+
+def find_closest_number(lst, num):
+    return min(lst, key=lambda x: (abs(x - num), x))
+
+
 def resample_data():
     parser = argparse.ArgumentParser(description="Resample image and mask")
-    parser.add_argument("--excel_path", type=str, required=True, help="Origin sorted images and masks excel file path.")
-    parser.add_argument("--out_path", type=str, required=True, help="Output resampled images and masks excel file path.")
+    parser.add_argument("--excel_path", type=str, default="/home/huangdn/Causal3D-Net/src/dataset/dataset.xlsx", help="Origin sorted images and masks excel file path.")
+    parser.add_argument("--out_path", type=str, default="/home/huangdn/Causal3D-Net/src/data", help="Output resampled images and masks dir path.")
+    # parser.add_argument("--excel_path", type=str, required=True, help="Origin sorted images and masks Excel file path.")
+    # parser.add_argument("--out_path", type=str, required=True, help="Output resampled images and masks dir path.")
+    parser.add_argument("--resample_num", type=int, choices=[1, 3, 5], default=5, help="Total after resampling. Choose from 1, 3, or 5.")
     args = parser.parse_args()
 
     dataset_excel = pd.read_excel(args.excel_path)
@@ -69,6 +91,17 @@ def resample_data():
     masks_save_path = os.path.join(args.out_path, "masks")
     os.makedirs(images_save_path, exist_ok=True)
     os.makedirs(masks_save_path, exist_ok=True)
+    z_spacing_list = get_z_spacing_list(args.resample_num)
+    for index, row in dataset_excel.iterrows():
+        image_path = row['image_path']
+        mask_path = row['mask_path']
+        cancer = row['cancer']
+        image_nii = nib.load(image_path)
+        mask_nii = nib.load(mask_path)
+        origin_spacing = abs(image_nii.affine[2, 2])
+        print(origin_spacing, find_closest_number(z_spacing_list, origin_spacing))
+        if index == 1:
+            break
 
     # print(f"{images_save_path} ===== {masks_save_path}")
     pass
