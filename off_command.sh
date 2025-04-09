f7f5195d647ee7d3634d27a40fd286b2fc7dfbca
CarlCypress <CarlCypress@yeah.net>
2025-04-09 14:33:39 +0800

    multi process resample data

---
+++ off_command.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+/home/huangdn/anaconda/envs/Causal3DNet/bin/python ./src/preprocessing/resample_data.py --excel_path /home/huangdn/Causal3D-Net/src/dataset/dataset.xlsx --out_path /home/huangdn/Causal3D-Net/src/data
---
+++ src/test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/3/24 18:42
+# @Author  : D.N. Huang
+# @Email   : CarlCypress@yeah.net
+# @FileName: test.py
+# @Project : Causal3D-Net
