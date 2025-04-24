f8ca7f77a30de7726ff53e9a5240019fe520f60c
CarlCypress <CarlCypress@yeah.net>
2025-04-24 22:08:36 +0800

    check resampled mask type

--- off_command.sh
+++ off_command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-/home/huangdn/anaconda/envs/Causal3DNet/bin/python ./src/preprocessing/individual_confounders.py --input /home/huangdn/Causal3D-Net/src/dataset/radiomics_read.xlsx --output /home/huangdn/Causal3D-Net/src/data/radiomics_features.xlsx
+/home/huangdn/anaconda/envs/Causal3DNet/bin/python ./src/utils/check_mask.py
--- src/utils/check_mask.py
+++ src/utils/check_mask.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import nibabel as nib
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
 
 def print_mask_type(file_path):
@@ -42,7 +43,7 @@ def mask_volume_box(file_path):
     min_corner_coords = min_corner.astype(float)  # 转为float类型
     max_corner_coords = max_corner.astype(float)  # 转为float类型
     # 将所有信息整理成一个列表返回
-    result = list(shape) + list(box_shape) + [mask_volume] + min_corner_coords.tolist() + max_corner_coords.tolist()
+    result = [file_path] + list(shape) + list(box_shape) + [mask_volume] + min_corner_coords.tolist() + max_corner_coords.tolist()
     return result
 
 
@@ -50,30 +51,36 @@ def mask_volume_box(file_path):
 def through_files():
     excel_path = "/home/huangdn/Causal3D-Net/src/data/data_finger.xlsx"
     # logging.basicConfig(
-    #     filename="/home/huangdn/Causal3D-Net/src/logging_record/mask_size_record.log",
+    #     filename="/home/huangdn/Causal3D-Net/src/logging_record/mask_type.log",
     #     level=logging.INFO,
     #     format="%(asctime)s - %(message)s",
     #     filemode='w'
     # )
     df = pd.read_excel(excel_path)
-    results = []
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        mask_path = row["mask_path"]
-        # 获取每个mask的相关数据
-        result = mask_volume_box(mask_path)
-        # 在结果前面加上文件名
-        result_with_filename = [mask_path] + result
-        # 将结果添加到results列表中
-        results.append(result_with_filename)
-    # 将所有结果转化为DataFrame
-    results_df = pd.DataFrame(results, columns=[
-        "file_name", "width", "height", "depth", "box_width", "box_height", "box_depth",
-        "mask_count", "min_x", "min_y", "min_z", "max_x", "max_y", "max_z"
-    ])
-    # 将结果保存为Excel文件
-    output_path = "/home/huangdn/Causal3D-Net/src/logging_record/mask_volume_info.xlsx"
-    results_df.to_excel(output_path, index=False)
-    print(f"Results saved to {output_path}")
+         mask_path = row["mask_path"]
+         print_mask_type(mask_path)
+    pass
+    # results = []
+    # with ProcessPoolExecutor(max_workers=4) as executor:  # 设置最大进程数为4
+    #     results = list(tqdm(executor.map(mask_volume_box, df["mask_path"]), total=df.shape[0]))
+    # # for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+    # #     mask_path = row["mask_path"]
+    # #     # 获取每个mask的相关数据
+    # #     result = mask_volume_box(mask_path)
+    # #     # 在结果前面加上文件名
+    # #     result_with_filename = [mask_path] + result
+    # #     # 将结果添加到results列表中
+    # #     results.append(result_with_filename)
+    # # 将所有结果转化为DataFrame
+    # results_df = pd.DataFrame(results, columns=[
+    #     "file_name", "width", "height", "depth", "box_width", "box_height", "box_depth",
+    #     "mask_count", "min_x", "min_y", "min_z", "max_x", "max_y", "max_z"
+    # ])
+    # # 将结果保存为Excel文件
+    # output_path = "/home/huangdn/Causal3D-Net/src/logging_record/mask_volume_info.xlsx"
+    # results_df.to_excel(output_path, index=False)
+    # print(f"Results saved to {output_path}")
 
 
 if __name__ == '__main__':
