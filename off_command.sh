7ac6fd5b94e88ad0e6285ae90207a361f6c6b1bd
CarlCypress <CarlCypress@yeah.net>
2025-07-02 10:38:31 +0800

    docs: metric display

--- off_command.sh
+++ off_command.sh
@@ -4,7 +4,7 @@
 /home/huangdn/anaconda/envs/Causal3DNet/bin/python -m src.baselines.baseline_entry \
     --train /home/huangdn/Causal3D-Net/src/dataset/dataset_for_train.xlsx \
     --test /home/huangdn/Causal3D-Net/src/dataset/dataset_for_test.xlsx \
-    --method hybrid_transformer \
+    --method neural_transformer \
     --cuda_id 5 \
     --outdir /home/huangdn/Causal3D-Net/src/results
 
