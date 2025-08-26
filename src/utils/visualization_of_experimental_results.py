fea4ff8701d6e29c66dda4aa8bbc413d6538314d
CarlCypress <CarlCypress@yeah.net>
2025-08-26 20:52:47 +0800

    docs: model charts change

--- src/utils/visualization_of_experimental_results.py
+++ src/utils/visualization_of_experimental_results.py
@@ -16,7 +16,7 @@ def vis_10_folds_cv(
         save_path="/home/huangdn/Causal3D-Net/src/logging_record/10_folds_cross_validation_results.png"
 ):
     # 设置字体
-    plt.rcParams['font.family'] = 'Times New Roman'
+    # plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams['font.size'] = 15
 
     # 读取 CSV
@@ -62,9 +62,69 @@ def vis_10_folds_cv(
     plt.show()
 
 
-def vis_ablation():
 
-    pass
+def vis_ablation(excel_path="/home/huangdn/Causal3D-Net/src/logging_record/ablation.xlsx",
+                 save_path="/home/huangdn/Causal3D-Net/src/logging_record/model_ablation.png"):
+
+    plt.rcParams['font.size'] = 15
+
+    order = [
+        "baseline",
+        "indi",
+        "indi+ot",
+        "cent",
+        "cent+ot",
+        "indi+cent",
+        "indi+cent+ot"
+    ]
+
+    excel = pd.read_excel(excel_path).iloc[:, :-1]
+    excel = excel[["name", "dataset", "Accuracy"]]
+
+    datasets = excel["dataset"].unique()
+    n_dataset = len(datasets)
+    n_group = len(order)
+
+    # 控制参数
+    bar_width = 0.08     # 每根柱子的宽度
+    inner_gap = 0.02     # 组内柱子之间的间隔
+    group_gap = 0.2      # 组与组之间的间隔
+
+    # 每个 dataset 在 x 轴上的起始位置
+    group_width = n_group * bar_width + (n_group - 1) * inner_gap
+    x_positions = np.arange(n_dataset) * (group_width + group_gap)
+
+    palette = sns.color_palette("Set2", n_group)
+
+    plt.figure(figsize=(10, 6))
+
+    for i, name in enumerate(order):
+        data_i = excel[excel["name"] == name].set_index("dataset").loc[datasets]["Accuracy"].values
+        bar_x = x_positions + i * (bar_width + inner_gap)
+        plt.bar(bar_x,
+                data_i,
+                width=bar_width,
+                label=name,
+                color=palette[i],
+                edgecolor="black",
+                linewidth=0.5
+                )
+
+    # 设置x轴刻度在每组的中心
+    plt.xticks(x_positions + group_width/2 - bar_width/2, datasets)
+
+    plt.ylabel("ACC")
+    plt.ylim(0.65, 1)
+    plt.legend(
+        title="",
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.12),  # 横向居中，纵向往下
+        ncol=4,  # 图例分成多列，避免太宽
+        frameon=False  # 去掉外框（可选）
+    )
+    plt.tight_layout()
+    plt.savefig(save_path, dpi=600, bbox_inches='tight', transparent=False)
+    plt.show()
 
 
 if __name__ == '__main__':
